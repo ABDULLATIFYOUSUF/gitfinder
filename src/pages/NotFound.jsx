d0a62fcb348be59c3512f49b7708079d1240e932
latif <latifyousuf513@gmail.com>
2023-10-08 10:36:18 -0700

    first commit

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -4,9 +4,9 @@ import nfimage from '../assets/image404.png'
 function NotFound() {
   return (
     <>
-    <div className='w-full h-[80vh] flex flex-row items-center justify-center'>
-        <img className='w-96 h-96' src={nfimage} />
-      <h1 className='text-white text-9xl text-center'>Data Not Found</h1>
+    <div className='w-full h-[70vh] flex flex-col items-center justify-center'>
+      <h1 className='text-white text-6xl text-center'>404 <br /> Page Not Found</h1>
+      <img className='w-80 h-80 mt-9' src={nfimage} />
     </div>
     </>
     
